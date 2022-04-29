9987f12cb045ebe0020b393ad43805b1df064a9c
Bill Plein <260078+bplein@users.noreply.github.com>
2022-04-29 16:28:01 -0500

    Update rundemo.sh
    Remove namespace context change

--- rundemo.sh
+++ rundemo.sh
@@ -10,11 +10,9 @@ source ./util.sh
 
 export namespace=postgres-demo
 desc ""
-desc "First lets create a namespace to run our application and switch context to it"
+desc "First lets create a namespace to run our application"
 run "kubectl create ns $namespace"
 
-
-run "kubectl config set-context --current --namespace=$namespace"
 
 desc "Now lets configure and enable Autopilot"
 run "cat autopilot-configmap.yaml"
@@ -39,17 +37,17 @@ desc ""
 desc "Now create a volume for the application."
 
 run "cat px-postgres-pvc.yaml"
-run "kubectl apply -f px-postgres-pvc.yaml"
+run "kubectl -n $namespace apply -f px-postgres-pvc.yaml"
 
 echo -n postgres123 > password.txt
-kubectl create secret generic postgres-pass --from-file=password.txt 2>&1 >/dev/null
+kubectl -n $namespace create secret generic postgres-pass --from-file=password.txt 2>&1 >/dev/null
 
 
 desc ""
 desc "And now we'll take a look at the application in YAML format and deploy it (hit CTRL-C to stop watching the application when it's up)"
 run "cat postgres-app.yaml"
-run "kubectl create -f postgres-app.yaml"
-watch kubectl get pods -l app=postgres -o wide
+run "kubectl -n $namespace create -f postgres-app.yaml"
+watch kubectl -n $namespace get pods -l app=postgres -o wide
 
 #clear the screen
 clear
@@ -62,13 +60,13 @@ desc ""
 
 desc ""
 desc "We are going to exec into the Postgres pod and run a command to populate data, and then get the count"
-run "kubectl get pods -l app=postgres"
-POD=$(kubectl get pods -l app=postgres | grep Running | grep 1/1 | awk '{print $1}')
+run "kubectl -n $namespace get pods -l app=postgres"
+POD=$(kubectl -n $namespace get pods -l app=postgres | grep Running | grep 1/1 | awk '{print $1}')
 export POD
 desc "Our pod is called $POD"
 desc ""
 desc "Create the database"
-run "kubectl exec -i $POD -- psql << EOF
+run "kubectl -n $namespace exec -i $POD -- psql << EOF
 create database pxdemo;
 \l
 \q
@@ -76,7 +74,7 @@ EOF"
 
 desc ""
 desc "Populate the database with test data"
-run "kubectl exec -i $POD -- pgbench -i -s 50 pxdemo;"
+run "kubectl -n $namespace exec -i $POD -- pgbench -i -s 50 pxdemo;"
 
 ##########
 # get count
@@ -84,14 +82,14 @@ run "kubectl exec -i $POD -- pgbench -i -s 50 pxdemo;"
 desc ""
 desc "Let's get the count of records from the database table"
 
-POD=$(kubectl get pods -l app=postgres | grep Running | grep 1/1 | awk '{print $1}')
+POD=$(kubectl -n $namespace get pods -l app=postgres | grep Running | grep 1/1 | awk '{print $1}')
 export POD
-run "kubectl exec -i $POD -- psql pxdemo<< EOF
+run "kubectl -n $namespace exec -i $POD -- psql pxdemo<< EOF
 select count(*) from pgbench_accounts;
 \q
 EOF"
 ##########
-watch kubectl get pvc -n postgres-demo
+watch kubectl -n $namespace get pvc -n postgres-demo
 
 
 
