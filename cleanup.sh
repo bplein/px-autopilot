3f21e4e5d432b54fee158c7d6ab00189aedb3427
Bill Plein <260078+bplein@users.noreply.github.com>
2022-04-29 16:48:40 -0500

    Update cleanup.sh
    More cleanup of cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -7,7 +7,7 @@ echo "This will destroy everything in namespace $namespace!!!!"
 echo " "
 read -p "Hit CTRL-C to escape, press ENTER to continue... " -n1
 
-kubectl -n portworx patch configmap autopilot-config --type=merge --patch '{"data":{"config.yaml":"providers:\n   - name: default\n     type: prometheus\n     params: url=http://px-prometheus:9090\nmin_poll_interval: 10"}}'
-kubectl -n portworx delete -f grow-pvc-rule.yaml
+kubectl -n portworx patch configmap autopilot-config --type=merge --patch '{"data":{"config.yaml":"providers:\n   - name: default\n     type: prometheus\n     params: url=http://px-prometheus:9090\nmin_poll_interval: []"}}'
+kubectl delete -f grow-pvc-rule.yaml
 kubectl delete ns $namespace
 kubectl delete sc px-repl3-sc-demotemp
